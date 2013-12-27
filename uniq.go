00930b8d8e6e630e3f71032ebfa8719d96ef62f8
Joe <infogulch@gmail.com>
2013-12-26 21:48:09 -0600

    Create uniq.go
    Use like: `a[:uniq.Ue(a)]`

---
+++ uniq.go
@@ -0,0 +1,23 @@
+package uniq
+
+import "sort"
+
+type Interface sort.Interface
+
+func Ue(data Interface) int {
+	len := data.Len()
+	i, j := 0, 1
+	// find the first duplicate
+	for j < len && data.Less(i, j) {
+		i++
+		j++
+	}
+	// this loop is simpler after the first duplicate is found
+	for ; j < len; j++ {
+		if data.Less(i, j) {
+			i++
+			data.Swap(i, j)
+		}
+	}
+	return i + 1
+}
