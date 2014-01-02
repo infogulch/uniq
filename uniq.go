176b69308a174677b2c43e2aae76f071a014c96c
infogulch <infogulch@gmail.com>
2014-01-02 00:52:52 -0600

    Add Stable(): duplicates are stable too, but O(n log n)

--- uniq.go
+++ uniq.go
@@ -27,3 +27,63 @@ func Uniq(data Interface) int {
 	}
 	return i + 1
 }
+
+func Stable(data Interface) int {
+	return stable(data, 0, data.Len())
+}
+
+func stable(data Interface, start, end int) int {
+	if n := end - start; n <= 2 {
+		if n == 2 && !data.Less(start, start+1) {
+			n--
+		}
+		return n
+	}
+	mid := start + (end-start)/2 // average safe from overflow
+	ua := stable(data, start, mid)
+	ub := stable(data, mid, end)
+	if ua > 0 && ub > 0 && !data.Less(start+ua-1, mid) {
+		mid++ // the first element in B is present in a
+		ub--
+	}
+	shift(data, start+ua, mid, mid+ub)
+	return ua + ub
+}
+
+// shift exchanges elements in a sort.Interface from range [start,mid) with those in range [mid,end).
+// Performs (end-start) swaps and accesses each element twice in the worst & average case, and (end-start)/2 swaps and accesses each element once in the best case.
+func shift(data Interface, start, mid, end int) {
+	if start >= mid || mid >= end {
+		return // no elements to shift
+	}
+	if mid-start == end-mid {
+		// equal sizes, use faster algorithm
+		swapn(data, start, mid, mid-start)
+		return
+	}
+	reverse(data, start, mid)
+	reverse(data, mid, end)
+	reverse(data, start, end)
+}
+
+// reverse transposes elements in a sort.Interface so that the elements in range [start,end) are in reverse order.
+// Performs (end-start)/2 swaps, and acceses each element from start to end once.
+func reverse(data Interface, start, end int) {
+	end--
+	for start < end {
+		data.Swap(start, end)
+		start++
+		end--
+	}
+}
+
+// swapn transposes the elements in two sections of equal length in a sort.Interface.
+// The sections start at indices i & j.
+// If the sections overlap (i.e. min(i,j)+len > max(i,j)) the result is undefined.
+// Performs len swaps and acceses each element once.
+func swapn(data Interface, i, j, n int) {
+	for n > 0 {
+		n--
+		data.Swap(i+n, j+n)
+	}
+}
--- uniq_test.go
+++ uniq_test.go
@@ -4,18 +4,51 @@ import "math/rand"
 import "sort"
 import "testing"
 
-func TestUniqEmpty(t *testing.T) {
+type uniquerFunc func(Interface) int
+
+func (f uniquerFunc) unique(i Interface) int {
+	return f(i)
+}
+
+type uniquer struct {
+	unique uniquerFunc
+}
+
+// Generic test instances below
+func TestUniqEmpty(t *testing.T)   { uniquer{Uniq}.testUniqEmpty(t) }
+func TestStableEmpty(t *testing.T) { uniquer{Stable}.testUniqEmpty(t) }
+
+func TestUniqOne(t *testing.T)   { uniquer{Uniq}.testUniqOne(t) }
+func TestStableOne(t *testing.T) { uniquer{Stable}.testUniqOne(t) }
+
+func TestUniqTiny(t *testing.T)   { uniquer{Uniq}.testUniqTiny(t) }
+func TestStableTiny(t *testing.T) { uniquer{Stable}.testUniqTiny(t) }
+
+func TestUniqSmall(t *testing.T)   { uniquer{Uniq}.testUniqRandom(t, 30, 1000) }
+func TestStableSmall(t *testing.T) { uniquer{Stable}.testUniqRandom(t, 30, 1000) }
+
+func TestUniqMedium(t *testing.T)   { uniquer{Uniq}.testUniqRandom(t, 1000, 10) }
+func TestStableMedium(t *testing.T) { uniquer{Stable}.testUniqRandom(t, 1000, 10) }
+
+func TestUniqLarge(t *testing.T)   { uniquer{Uniq}.testUniqRandom(t, 10000, 2) }
+func TestStableLarge(t *testing.T) { uniquer{Stable}.testUniqRandom(t, 10000, 2) }
+
+func TestUniqAllDuplicate(t *testing.T)   { uniquer{Uniq}.testUniqAllDuplicate(t) }
+func TestStableAllDuplicate(t *testing.T) { uniquer{Stable}.testUniqAllDuplicate(t) }
+
+// Generic tests that apply to all uniquer funcs
+func (q uniquer) testUniqEmpty(t *testing.T) {
 	a := []int{}
-	a = a[:Uniq(sort.IntSlice(a))]
+	a = a[:q.unique(sort.IntSlice(a))]
 	if len(a) != 0 {
 		t.Errorf("Congratulations, you broke loops")
 	}
 }
 
-func TestUniqOne(t *testing.T) {
+func (q uniquer) testUniqOne(t *testing.T) {
 	const value int = 100
 	a := []int{value}
-	a = a[:Uniq(sort.IntSlice(a))]
+	a = a[:q.unique(sort.IntSlice(a))]
 	if len(a) != 1 {
 		t.Errorf("Wrong length")
 	} else if a[0] != value {
@@ -23,26 +56,14 @@ func TestUniqOne(t *testing.T) {
 	}
 }
 
-func TestUniqTiny(t *testing.T) {
+func (q uniquer) testUniqTiny(t *testing.T) {
 	a := []int{1, 2, 2, 2, 3, 4, 4, 4, 4, 4, 5, 5, 6, 6, 7, 8, 9}
 	u := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	a = a[:Uniq(sort.IntSlice(a))]
+	a = a[:q.unique(sort.IntSlice(a))]
 	compareIntSlice(a, u, t)
 }
 
-func TestUniqSmall(t *testing.T) {
-	testUniqRandom(t, 30, 1000)
-}
-
-func TestUniqMedium(t *testing.T) {
-	testUniqRandom(t, 1000, 10)
-}
-
-func TestUniqLarge(t *testing.T) {
-	testUniqRandom(t, 10000, 2)
-}
-
-func testUniqRandom(t *testing.T, size, count int) {
+func (q uniquer) testUniqRandom(t *testing.T, size, count int) {
 	rand.Seed(0) // specify seed for determinism
 	u, a := make([]int, size), make([]int, 100*size)
 	for i, _ := range u {
@@ -53,18 +74,18 @@ func testUniqRandom(t *testing.T, size, count int) {
 			a[i] = rand.Intn(size)
 		}
 		sort.Sort(sort.IntSlice(a))
-		b := a[:Uniq(sort.IntSlice(a))]
+		b := a[:q.unique(sort.IntSlice(a))]
 		compareIntSlice(b, u, t)
 	}
 }
 
-func TestUniqAllDuplicate(t *testing.T) {
+func (q uniquer) testUniqAllDuplicate(t *testing.T) {
 	a := make([]int, 1000)
 	const value int = 123
 	for i, _ := range a {
 		a[i] = value
 	}
-	a = a[:Uniq(sort.IntSlice(a))]
+	a = a[:q.unique(sort.IntSlice(a))]
 	if len(a) != 1 {
 		t.Errorf("Didn't eliminate all duplicates. len should be 1: %d", len(a))
 	} else if a[0] != value {
