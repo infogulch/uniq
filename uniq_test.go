8b5ca3e3f11a65b24e83233b7501fae2e285f7f3
infogulch <infogulch@gmail.com>
2014-01-02 20:03:01 -0600

    Rearrange tests

--- uniq_test.go
+++ uniq_test.go
@@ -9,29 +9,6 @@ import (
 
 type uniquer func(uniq.Interface) int
 
-// Generic test instances below
-func TestUniqEmpty(t *testing.T)   { testUniqEmpty(uniq.Uniq, t) }
-func TestStableEmpty(t *testing.T) { testUniqEmpty(uniq.Stable, t) }
-
-func TestUniqOne(t *testing.T)   { testUniqOne(uniq.Uniq, t) }
-func TestStableOne(t *testing.T) { testUniqOne(uniq.Stable, t) }
-
-func TestUniqTiny(t *testing.T)   { testUniqTiny(uniq.Uniq, t) }
-func TestStableTiny(t *testing.T) { testUniqTiny(uniq.Stable, t) }
-
-func TestUniqSmall(t *testing.T)   { testUniqRandom(uniq.Uniq, t, 30, 1000) }
-func TestStableSmall(t *testing.T) { testUniqRandom(uniq.Stable, t, 30, 1000) }
-
-func TestUniqMedium(t *testing.T)   { testUniqRandom(uniq.Uniq, t, 1000, 10) }
-func TestStableMedium(t *testing.T) { testUniqRandom(uniq.Stable, t, 1000, 10) }
-
-func TestUniqLarge(t *testing.T)   { testUniqRandom(uniq.Uniq, t, 10000, 2) }
-func TestStableLarge(t *testing.T) { testUniqRandom(uniq.Stable, t, 10000, 2) }
-
-func TestUniqAllDuplicate(t *testing.T)   { testUniqAllDuplicate(uniq.Uniq, t) }
-func TestStableAllDuplicate(t *testing.T) { testUniqAllDuplicate(uniq.Stable, t) }
-
-// Generic tests that apply to all uniquer funcs
 func testUniqEmpty(unique uniquer, t *testing.T) {
 	a := []int{}
 	a = a[:unique(sort.IntSlice(a))]
@@ -39,6 +16,9 @@ func testUniqEmpty(unique uniquer, t *testing.T) {
 		t.Errorf("Congratulations, you broke loops")
 	}
 }
+
+func TestUniqEmpty(t *testing.T)   { testUniqEmpty(uniq.Uniq, t) }
+func TestStableEmpty(t *testing.T) { testUniqEmpty(uniq.Stable, t) }
 
 func testUniqOne(unique uniquer, t *testing.T) {
 	const value int = 100
@@ -51,12 +31,18 @@ func testUniqOne(unique uniquer, t *testing.T) {
 	}
 }
 
+func TestUniqOne(t *testing.T)   { testUniqOne(uniq.Uniq, t) }
+func TestStableOne(t *testing.T) { testUniqOne(uniq.Stable, t) }
+
 func testUniqTiny(unique uniquer, t *testing.T) {
 	a := []int{1, 2, 2, 2, 3, 4, 4, 4, 4, 4, 5, 5, 6, 6, 7, 8, 9}
 	u := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	a = a[:unique(sort.IntSlice(a))]
 	compareIntSlice(a, u, t)
 }
+
+func TestUniqTiny(t *testing.T)   { testUniqTiny(uniq.Uniq, t) }
+func TestStableTiny(t *testing.T) { testUniqTiny(uniq.Stable, t) }
 
 func testUniqRandom(unique uniquer, t *testing.T, size, count int) {
 	rand.Seed(0) // specify seed for determinism
@@ -74,6 +60,15 @@ func testUniqRandom(unique uniquer, t *testing.T, size, count int) {
 	}
 }
 
+func TestUniqSmall(t *testing.T)   { testUniqRandom(uniq.Uniq, t, 30, 1000) }
+func TestStableSmall(t *testing.T) { testUniqRandom(uniq.Stable, t, 30, 1000) }
+
+func TestUniqMedium(t *testing.T)   { testUniqRandom(uniq.Uniq, t, 1000, 10) }
+func TestStableMedium(t *testing.T) { testUniqRandom(uniq.Stable, t, 1000, 10) }
+
+func TestUniqLarge(t *testing.T)   { testUniqRandom(uniq.Uniq, t, 10000, 2) }
+func TestStableLarge(t *testing.T) { testUniqRandom(uniq.Stable, t, 10000, 2) }
+
 func testUniqAllDuplicate(unique uniquer, t *testing.T) {
 	a := make([]int, 1000)
 	const value int = 123
@@ -87,6 +82,9 @@ func testUniqAllDuplicate(unique uniquer, t *testing.T) {
 		t.Errorf("Changed values. Should be %d: %d", value, a[0])
 	}
 }
+
+func TestUniqAllDuplicate(t *testing.T)   { testUniqAllDuplicate(uniq.Uniq, t) }
+func TestStableAllDuplicate(t *testing.T) { testUniqAllDuplicate(uniq.Stable, t) }
 
 // types for stability tests
 
